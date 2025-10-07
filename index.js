dff5840296e3e220a5eca2a8c66b7047de895877
Ethan Carranza Serrano <ethan.carranza@fero.com>
2025-10-07 20:15:19 +0200

    Revert "Update server export and Vercel config for deployment"
    This reverts commit 1a7d9534e812b3c3f4a95b544e5a0eea98a54fe8.

--- index.js
+++ index.js
@@ -49,11 +49,6 @@ app.use("*", (req, res, next) => {
 });
 
 const PORT = process.env.PORT || 4200;
-
-if (process.env.NODE_ENV !== 'production') {
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-  });
-}
-
-module.exports = app;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
